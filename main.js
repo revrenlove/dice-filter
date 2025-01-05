2679994e9aadcf811afba4ecdb3a119ad7590485
revrenlove <58348549+revrenlove@users.noreply.github.com>
2025-01-04 18:10:32 -0600

    adding company filter

--- main.js
+++ main.js
@@ -3,24 +3,31 @@ if (!allHiddenArticles) {
 }
 
 {
-    console.info("The Dice Filter Extension has been activated...");
+    const companiesToIgnore = [
+        'Targeted Talent',
+        'Jobot',
+        'Robert Half',
+        'Motion Recruitment',
+    ];
 
-    const hideListing = (searchCard) => searchCard.classList.add("hidden");
+    console.info('The Dice Filter Extension has been activated...');
+
+    const hideListing = (searchCard) => searchCard.classList.add('hidden');
 
     const getKey = (searchCard) =>
-        searchCard.querySelector("div.card-header a.card-title-link").id;
+        searchCard.querySelector('div.card-header a.card-title-link').id;
 
     const addHideButton = (searchCard) => {
-        const p = document.createElement("p");
+        const p = document.createElement('p');
 
-        p.innerHTML = "Hide Listing";
-        p.classList.add("z-10");
-        p.classList.add("hide-listing");
+        p.innerHTML = 'Hide Listing';
+        p.classList.add('z-10');
+        p.classList.add('hide-listing');
 
-        p.addEventListener("click", () => {
+        p.addEventListener('click', () => {
             const key = getKey(searchCard);
 
-            localStorage.setItem(key, "RevrenLove");
+            localStorage.setItem(key, 'RevrenLove');
             hideListing(searchCard);
             allHiddenArticles.push(key);
         });
@@ -29,10 +36,20 @@ if (!allHiddenArticles) {
     };
 
     // TODO: JE - This is where the shit actually starts... move it to the top
-    document.querySelectorAll("dhi-search-card").forEach((searchCard) => {
+    document.querySelectorAll('dhi-search-card').forEach((searchCard) => {
         const key = getKey(searchCard);
 
         if (localStorage.getItem(key) || allHiddenArticles.includes(key)) {
+            hideListing(searchCard);
+
+            return;
+        }
+
+        const companyName = searchCard
+            .querySelector('.card-company a')
+            .innerHTML.trim();
+
+        if (companiesToIgnore.includes(companyName)) {
             hideListing(searchCard);
 
             return;
