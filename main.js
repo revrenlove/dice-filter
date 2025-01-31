65bfc73a0df5824fbe3b04ddedf79b1a9758f7c4
revrenlove <58348549+revrenlove@users.noreply.github.com>
2025-01-30 18:38:45 -0600

    adding keywords to ignore

--- main.js
+++ main.js
@@ -11,13 +11,27 @@ if (!allHiddenArticles) {
         'Providence Health & Services',
     ];
 
+    const keywordsToIgnore = ['ui path', 'uipath', 'embedded', 'clearance'];
+
     console.info('The Dice Filter Extension has been activated...');
 
+    /**
+     *
+     * @param {Element} searchCard
+     */
     const hideListing = (searchCard) => searchCard.classList.add('hidden');
 
+    /**
+     *
+     * @param {Element} searchCard
+     */
     const getKey = (searchCard) =>
         searchCard.querySelector('div.card-header a.card-title-link').id;
 
+    /**
+     *
+     * @param {Element} searchCard
+     */
     const addHideButton = (searchCard) => {
         const p = document.createElement('p');
 
@@ -36,7 +50,22 @@ if (!allHiddenArticles) {
         searchCard.appendChild(p);
     };
 
-    // TODO: JE - This is where the shit actually starts... move it to the top
+    /**
+     *
+     * @param {string} text
+     */
+    const textContainsIgnoredKeyword = (text) => {
+        text = text.toLowerCase();
+
+        for (const keyword of keywordsToIgnore) {
+            if (text.indexOf(keyword) != -1) {
+                return true;
+            }
+        }
+
+        return false;
+    };
+
     document.querySelectorAll('dhi-search-card').forEach((searchCard) => {
         const key = getKey(searchCard);
 
@@ -46,11 +75,29 @@ if (!allHiddenArticles) {
             return;
         }
 
+        // TODO: JE - Separate these into their own functions...
         const companyName = searchCard
             .querySelector('.card-company a')
             .innerHTML.trim();
 
         if (companiesToIgnore.includes(companyName)) {
+            hideListing(searchCard);
+
+            return;
+        }
+
+        const jobTitle = searchCard
+            .querySelector('.card-title-link')
+            .innerHTML.trim();
+
+        const jobDescription = searchCard
+            .querySelector('.card-description')
+            .innerHTML.trim();
+
+        if (
+            textContainsIgnoredKeyword(jobTitle) ||
+            textContainsIgnoredKeyword(jobDescription)
+        ) {
             hideListing(searchCard);
 
             return;
